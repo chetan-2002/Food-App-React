78bf13a37a4f0e991a940cd36a672ce869a26aab
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-11-09 12:32:37 +0530

    new files added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React , {useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+  const cartCtx =useContext(CartContext);
+  const numberOfItems = cartCtx.items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
   return (
-    <button className = {styles.button}>
+    <button className = {styles.button} onClick = {props.onClick}>
       <span className = {styles.icon}>
         <CartIcon></CartIcon>
       </span>
       <span>Your cart</span>
-      <span className = {styles.badge}>3</span>
+      <span className = {styles.badge}>{numberOfItems}</span>
     </button>
   );
 };
