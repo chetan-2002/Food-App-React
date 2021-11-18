8325a51a4a0207b41b005719b5028e8daa84a131
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2021-11-18 20:12:40 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
 function App() {
-  const [cartIsShown , setCartIsShown] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose ={hideCartHandler}/>}
-      <Header onShowCart = {showCartHandler}/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals></Meals>
       </main>
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,46 +1,91 @@
+import { useEffect, useState } from "react";
 import styles from "./AvailableMeals.module.css";
 import Card from "../../components/UI/Card";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 const AvailableMeals = () => {
   //   const mealItems = DUMMY_MEALS.map((meal) => {
   //     <li>{meal.name}</li>;
   //   });
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://food-ordering-app-df053-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          ...responseData[key],
+          id: key,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchData().catch((err) => {
+      setIsLoading(false);
+      setHttpError(err.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  if (httpError) {
+    return (
+      <section className={styles.MealsError}>
+        <p>Failed to Load.Please try again.</p>
+      </section>
+    );
+  }
   return (
     <section className={styles.meals}>
-        <Card>
-      <ul>
-        {DUMMY_MEALS.map((meal) => (
-          <MealItem key={meal.id} meal={meal} />
-        ))}
-      </ul>
+      <Card>
+        <ul>
+          {meals.map((meal) => (
+            <MealItem key={meal.id} meal={meal} />
+          ))}
+        </ul>
       </Card>
     </section>
-  ); 
+  );
 };
 export default AvailableMeals;
